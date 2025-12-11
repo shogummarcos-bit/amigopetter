d86e87d51771a064f3927a0bd4f68273a6f6d2b9
shogummarcos-bit <shogummarcos@gmail.com>
2025-12-11 00:35:18 +0000

    commit

--- amigopetter/src/main/java/com/senai/amigopetter/service/AdocaoService.java
+++ amigopetter/src/main/java/com/senai/amigopetter/service/AdocaoService.java
@@ -2,11 +2,26 @@ package com.senai.amigopetter.service;
 
 import com.senai.amigopetter.models.Adocao;
 
+#Service
 public class AdocaoService {
-
-    public Adocao criarAdocao(Adocao adocao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'criarAdocao'");
-    }
+@Autowired
+pricate AdocaoRepositorio adocaoRepository;
+@Autowiredpriate PetsServices petsServices;
+public adocao criaradocao(Adocao adocao){
+    Aocao novaAdocao=adocaoRepository.save(adocao);
+    Pt pet=adocao.getPet();
+    return novaAdocao;
+}
+public Lis<Adocao>listartodasAdocoes(){
+    return adocaoRepository.findAll();
+}
+public adocao buscarAdocaoId(Long id){
+    return adocaoRepository.findById(id);
+}
+public void rmovrAdocao(Longid){
+    adocaoRepository.deleteById(id);
+    
+}
+   
 
 }
